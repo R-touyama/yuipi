d96ba6f2b1492ea9c8732fd372ae9900d3fbee0d
當山龍太朗 <r-toyama@slj.jp>
2024-04-14 17:15:09 +0900

    選手を取得する処理と構造化する処理を一つにまとめて関数化する

--- app.py
+++ app.py
@@ -16,20 +16,25 @@ from ansers import players
 # 正解したら次のクイズに進む
 
 
-# 選手の画像をランダムに取得する
-def get_random_players_and_img_path():
+# 選手の画像と選手名をペアに取得し、構造化して返す引数は取得する選手の数
+def get_random_players_and_img_path(num=4) -> dict:
     """
-    選手名と画像のパスをランダムに4人取得する
-    ex.
-    [
-        ["選手名", "画像のパス"],
-        ["選手名", "画像のパス"],
-        ["選手名", "画像のパス"],
-        ["選手名", "画像のパス"]
-    ]
+    選手名と画像のパスをランダムに4人取得し、構造化して返す
     """
     random.seed()  # シード値をリセット
-    return random.sample(list(players.items()), 4)
+    get_random_players_and_img_path = random.sample(list(players.items()), num)
+
+    return {
+        "answer_player": {
+            "name": get_random_players_and_img_path[0][0],
+            "img_path": get_random_players_and_img_path[0][1],
+        },
+        "fail_players": [
+            {"name": get_random_players_and_img_path[1][0], "img_path": get_random_players_and_img_path[1][1]},
+            {"name": get_random_players_and_img_path[2][0], "img_path": get_random_players_and_img_path[2][1]},
+            {"name": get_random_players_and_img_path[3][0], "img_path": get_random_players_and_img_path[3][1]},
+        ],
+    }
 
 
 # 画像をBase64に変換する
@@ -41,65 +46,59 @@ def get_image_base64(image_path):
 # def main():
 st.title("🏆⚽️プレミアリーグ選手クイズ⚽️🏆")
 
-# Streamlitのセッション状態を取得または初期化
-if "option_list" not in st.session_state:
-    st.session_state["option_list"] = get_random_players_and_img_path()
+# 初回だけここに入る初期化処理
+if (
+    "option_list" not in st.session_state
+    and "score" not in st.session_state
+    and "random_selectbox_value" not in st.session_state
+):
+    # 選択肢に出す選手を取得
+    st.session_state["option_list"] = get_random_players_and_img_path(4)
+    # スコアを初期化
     st.session_state["score"] = 0
+    # 選択肢をランダムにする
+    selectbox_list = [st.session_state["option_list"]["answer_player"]["name"]] + [
+        player["name"] for player in st.session_state["option_list"]["fail_players"]
+    ]
+    st.session_state["random_selectbox_value"] = random.sample(selectbox_list, 4)
 
-option_list = st.session_state["option_list"]
+# option_list = st.session_state["option_list"]
 
-# 選手名の構造化
-players_dict = {
-    "answer_player": {
-        "name": option_list[0][0],
-        "img_path": option_list[0][1],
-    },
-    "fail_players": [
-        {"name": option_list[1][0], "img_path": option_list[1][1]},
-        {"name": option_list[2][0], "img_path": option_list[2][1]},
-        {"name": option_list[3][0], "img_path": option_list[3][1]},
-    ],
-}
 
 image_html = '<div style="display: flex; justify-content: center;">'
-image_html += (
-    f'<img src="data:image/png;base64,{get_image_base64(players_dict["answer_player"]["img_path"])}" width="300"/>'
-)
+image_html += '<img src="data:image/png;base64,'
+image_html += f'{get_image_base64(st.session_state["option_list"]["answer_player"]["img_path"])}" width="300"/>'
 image_html += "</div>"
 
 st.markdown(image_html, unsafe_allow_html=True)
 
-# st.write(f"{st.session_state['score']} 問連続正解中🎉🎉🎉")
 st.markdown(
     f"<h3 style='text-align: center;'>{st.session_state['score']} 問連続正解中🎉🎉🎉</h1>", unsafe_allow_html=True
 )
 
-# 仕様の説明：streamlitの仕様で、selectboxを選択するとプログラムが再実行される。
-# バグ：選択肢の一番最初の要素が必ず正解になってしまう。
-# 解決したいこと：
-# 選択肢をこの段階でランダムにすると、ユーザーが選択肢を選択するたびに選択肢が変わり、
-# ユーザーの選択待ちになるため、ユーザーが選択しても選択肢が変わらないようにしたい。
-option = [players_dict["answer_player"]["name"]] + [player["name"] for player in players_dict["fail_players"]]
-
 # セレクトボックスで選択肢を表示
 user_select = st.selectbox(
     label="選手名はなんでしょう？？",
-    options=option,
+    options=st.session_state["random_selectbox_value"],
     index=None,
     placeholder="選手名を選択してください",
 )
 
 # 選択肢た選手が正解か不正解かを判定
 if user_select:
-    if user_select == players_dict["answer_player"]["name"]:
+    if user_select == st.session_state["option_list"]["answer_player"]["name"]:
         st.write("🎉🎉🎉正解です🎉🎉🎉")
         st.session_state["option_list"] = get_random_players_and_img_path()
         st.session_state["score"] += 1
+        # 正解したら選択肢をリセット
+        st.session_state["random_selectbox_value"] = None
     else:
-        st.write(f"😭😭😭不正解です😭😭😭 正解は{players_dict['answer_player']['name']}でした")
+        st.write(f"😭😭😭不正解です😭😭😭 正解は{st.session_state['option_list']['answer_player']['name']}でした")
         st.write(f"連続正解数は{st.session_state['score']}でした。\n 記録がリセットされます。")
         st.session_state["score"] = 0
         st.session_state["option_list"] = get_random_players_and_img_path()
+        # 不正解したら選択肢をリセット
+        st.session_state["random_selectbox_value"] = None
 
 # ページ再実行
 st.button("次の問題へ進む")
